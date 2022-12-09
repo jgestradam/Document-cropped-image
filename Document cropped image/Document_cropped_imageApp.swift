70b843ab57b571ceb644bd5f1cf74f717743a1b7
jgestradam <jgestradam@unal.edu.co>
2022-12-09 10:11:36 -0500

    Initial Commit

---
+++ Document cropped image/Document_cropped_imageApp.swift
@@ -0,0 +1,17 @@
+//
+//  Document_cropped_imageApp.swift
+//  Document cropped image
+//
+//  Created by Joseph Estrada on 12/9/22.
+//
+
+import SwiftUI
+
+@main
+struct Document_cropped_imageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
